0cc50143486b23ad501a5b8818b723988c327983
hoang <hoang390dbp@gmail.com>
2024-10-29 19:42:29 +0700

    fix app jsx bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import SidebarManager from "./components/manager/SidebarManager";
+import SidebarManager from "./components/manager/sidebar/SidebarManager";
 import ManagerProfile from "./pages/manager/profile/ManagerProfile";
 import ErrorPage from "./pages/error/ErrorPage";
 import Category from "./pages/manager/category/Category";
 import Product from "./pages/manager/product/Product";
+import AdminApp from './layout/AdminApp/AdminApp'
+import Total from './pages/manager/account/total/Total'
+import Customer from './pages/manager/account/customer/Customer'
+import Staff from './pages/manager/account/staff/Staff'
+
+import "bootstrap-icons/font/bootstrap-icons.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
   const router = createBrowserRouter([
