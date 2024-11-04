a2fd0cf33b148152b0bca2180efa8e2754bdf9a7
hoang <hoang390dbp@gmail.com>
2024-11-04 18:05:19 +0700

    dashboard ver1, add data for dashboard visuallization

--- src/pages/admin/AdminDashboard/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard/AdminDashboard.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, {useState , useEffect} from 'react'
+import axios from 'axios';
+import './AdminDashboard.css'
+
 import "./AdminDashboard.css"
 import PageTitle from '../AdditionalSections/PageTitle/PageTitle'
+import Card from '../AdditionalSections/Card/Card';
 
 const AdminDashboard = () => {
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/cards')
+      .then(response => setCards(response.data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
     <main id='main' className='main'>
       <PageTitle page="Dashboard"/>
 
+      <section className='dashboard section'>
+        <div className="row">
+          {
+            cards && cards.length > 0 &&
+            cards.map(card => <Card key={card._id} card={card}/>)
+          }
+        </div>
+      </section>
     </main>
   )
 }
