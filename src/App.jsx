3c7374e603299e468592414d6ac9aed352957203
PHUONG_KHANH\tranp <khanhtpse173570@fpt.edu.vn>
2024-11-05 09:46:54 +0700

    update navigation in HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
       children: [
         {
           index: true,
-          element: <Navigate to="/category/phone" replace />,
+          element: <Navigate to="/category/1" replace />,
         },
         {
           path: "category/:category",
