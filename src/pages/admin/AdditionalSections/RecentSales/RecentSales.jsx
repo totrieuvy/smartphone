34c4ed40a2c241eb7c071469717d25104a83de0d
hoang <hoang390dbp@gmail.com>
2024-11-21 16:24:57 +0700

    change data fetching into jsx, not fetch from json

--- src/pages/admin/AdditionalSections/RecentSales/RecentSales.jsx
+++ src/pages/admin/AdditionalSections/RecentSales/RecentSales.jsx
@@ -1,27 +1,16 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios';
-
+import React from 'react'
 import './RecentSales.css'
 import RecentSalesTable from '../RecentSalesTable/RecentSalesTable'
 
 const RecentSales = () => {
-    const [items, setItems] = useState([])
-    
-
-    useEffect(() => {
-        axios.get('http://localhost:4000/recentsales')
-          .then(response => setItems(response.data))
-          .catch(error => console.error('Error fetching data:', error));
-      }, []);
 
   return (
     <div className='card recent-sales overflow-auto'>
-    
       <div className="card-body">
         <h5 className="card-title">
             Recent Sales
         </h5>
-        <RecentSalesTable items={items}/>
+        <RecentSalesTable/>
       </div>
     </div>
   )
--- src/pages/admin/AdditionalSections/RecentSalesTable/RecentSalesTable.jsx
+++ src/pages/admin/AdditionalSections/RecentSalesTable/RecentSalesTable.jsx
@@ -1,45 +1,82 @@
-import React from 'react'
+import React from 'react';
 
-const RecentSalesTable = ({items}) => {
+const RecentSalesTable = () => {
+    const mockItems = [
+        {
+            _id: '1',
+            number: '#2644',
+            customer: 'Nguyễn Minh Hoàng',
+            product: 'Samsung Galaxy Z Flip6 5G 12GB/256GB',
+            price: 1299.99,
+            status: 'Approved',
+        },
+        {
+            _id: '2',
+            number: '#2457',
+            customer: 'Ngô Chí Kiên',
+            product: 'Samsung Galaxy Z Flip6 5G 12GB/256GB',
+            price: 799.99,
+            status: 'Pending',
+        },
+        {
+            _id: '3',
+            number: '#2147',
+            customer: 'Trần Phương Khánh',
+            product: 'Samsung Galaxy Z Flip6 5G 12GB/256GB',
+            price: 199.99,
+            status: 'Rejected',
+        },
+        {
+            _id: '4',
+            number: '#2049',
+            customer: 'Tô Triều Vỹ',
+            product: 'Samsung Galaxy Z Flip6 5G 12GB/256GB',
+            price: 49.99,
+            status: 'Approved',
+        },
+        {
+            _id: '5',
+            number: '#3592',
+            customer: 'Nguyễn Phương Nam',
+            product: 'Samsung Galaxy Z Flip6 5G 12GB/256GB',
+            price: 129.99,
+            status: 'Pending',
+        },
+    ];
+
     const handleStatus = status => {
-        switch(status){
+        switch (status) {
             case 'Approved':
                 return 'success';
-                break;
             case 'Pending':
                 return 'warning';
-                break;
             case 'Rejected':
                 return 'danger';
-                break;
             default:
                 return 'success';
         }
-    }
+    };
 
-  return (
-    <table className='table table-borderless datatable'>
-        <thead className='table-light'>
-            <tr>
-                <th scope='col'>#</th>
-                <th scope='col'>Customer</th>
-                <th scope='col'>Product</th>
-                <th scope='col'>Price</th>
-                <th scope='col'>Status</th>
-            </tr>
-        </thead>
-        <tbody>
-            {
-                items && 
-                items.length > 0 && 
-                items.map(item => (
+    return (
+        <table className="table table-borderless datatable">
+            <thead className="table-light">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Customer</th>
+                    <th scope="col">Product</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                {mockItems.map(item => (
                     <tr key={item._id}>
-                        <th scope='row'>
+                        <th scope="row">
                             <a href="#">{item.number}</a>
                         </th>
                         <td>{item.customer}</td>
                         <td>
-                            <a href="#" className='text-primary'>
+                            <a href="#" className="text-primary fw-bold">
                                 {item.product}
                             </a>
                         </td>
@@ -50,11 +87,10 @@ const RecentSalesTable = ({items}) => {
                             </span>
                         </td>
                     </tr>
-                ))
-            }
-        </tbody>
-    </table>
-  )
-}
+                ))}
+            </tbody>
+        </table>
+    );
+};
 
-export default RecentSalesTable
+export default RecentSalesTable;
